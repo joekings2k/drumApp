81ca8ada35d1d7cb9356e527cadb6bfb6e63723f
Your Name <eromoselejonthan@gmail.com>
2022-10-14 10:58:51 +0100

    internal changes

--- objectconstructro.js
+++ objectconstructro.js
@@ -13,3 +13,35 @@ function HouseKeper(yearsOfExperience,name,CleanigRepatoire,hobbies){
 
 let houseKeper1 = new HouseKeper(12,"Anny",["toilet","lobby"],["Dancing","Singing"])
 console.log(houseKeper1)
+let houseKepeer2  = new HouseKeper(20,"jane",["rooms","varanders"],"Sweeping")
+console.log(houseKepeer2)
+
+
+
+
+// class HouseKeper {
+//   constructor(yearsOfExperience, name, CleanigRepatoire, hobbies) {
+//     this.yearsOfExperience = yearsOfExperience;
+//     this.name = name;
+//     this.CleanigRepatoire = CleanigRepatoire;
+//     this.hobbies = hobbies;
+//     this.clean = function () {
+//       alert("cleaning in progress");
+//     };
+//   }
+// }
+
+// let houseKeper1 = new HouseKeper(
+//   12,
+//   "Anny",
+//   ["toilet", "lobby"],
+//   ["Dancing", "Singing"]
+// );
+// console.log(houseKeper1);
+// let houseKepeer2 = new HouseKeper(
+//   20,
+//   "jane",
+//   ["rooms", "varanders"],
+//   "Sweeping"
+// );
+// console.log(houseKepeer2);
